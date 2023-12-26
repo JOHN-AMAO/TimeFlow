cdd9cc701069179e8d385d1ae29ecde2ae019388
JOHN-AMAO <amaojohn767@gmail.com>
2023-12-26 02:38:36 +0100

    "Navbar created"

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default page;
