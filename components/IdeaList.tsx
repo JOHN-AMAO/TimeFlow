3fe6af004b49cf61aaca8831ea8464c2905ce6ad
JOHN-AMAO <amaojohn767@gmail.com>
2023-12-26 03:56:08 +0100

    ui done

--- components/IdeaList.tsx
+++ components/IdeaList.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export const IdeaList = () => {
   return (
@@ -18,12 +30,51 @@ export const IdeaList = () => {
                 condition, like at a particular breakpoint:
               </div>
               <div className='flex  gap-2 justify-end'>
-                <Button
-                  variant='outline'
-                  className='text-black bg-white hover:bg-slate-900'
-                >
-                  Edit
-                </Button>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant='outline'>Edit Idea</Button>
+                  </DialogTrigger>
+                  <DialogContent className=' bg-black'>
+                    <DialogHeader>
+                      <DialogTitle>Edit Idea</DialogTitle>
+                      <DialogDescription>
+                        Make changes to your ideas here. Click save when youre
+                        done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className='grid gap-4 py-4'>
+                      <div className='grid grid-cols-4 items-center gap-4'>
+                        <Label
+                          htmlFor='name'
+                          className='text-right'
+                        >
+                          Title
+                        </Label>
+                        <Input
+                          id='name'
+                          className='col-span-3 text-black'
+                        />
+                      </div>
+                      <div className='grid grid-cols-4 items-center gap-4'>
+                        <Label
+                          htmlFor='username'
+                          className='text-right'
+                        >
+                          Idea
+                        </Label>
+                        <Textarea className='text-black px-40' />
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <Button
+                        className='border rounded'
+                        type='submit'
+                      >
+                        Save changes
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
                 <Button
                   variant='outline'
                   className='text-black bg-white hover:bg-red-500'
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const Navbar = () => {
   return (
@@ -9,12 +21,55 @@ const Navbar = () => {
           <h1 className='text-2xl font-mono bg-gradient-to-r from-[rgb(48,223,72)] via-[#b609a7] to-[#077e64] text-transparent bg-clip-text animate-text font-bold'>
             Ideation
           </h1>
-          <Button
-            variant='outline'
-            className='bg-white text-black font-bold'
-          >
-            New Idea
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button
+                variant='outline'
+                className='bg-white text-black'
+              >
+                Create Idea
+              </Button>
+            </DialogTrigger>
+            <DialogContent className=' bg-black'>
+              <DialogHeader>
+                <DialogTitle>Save Idea</DialogTitle>
+                <DialogDescription>
+                  Create your ideas here. Click save when youre done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className='grid gap-4 py-4'>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label
+                    htmlFor='name'
+                    className='text-right'
+                  >
+                    Title
+                  </Label>
+                  <Input
+                    id='name'
+                    className='col-span-3 text-black'
+                  />
+                </div>
+                <div className='grid grid-cols-4 items-center gap-4'>
+                  <Label
+                    htmlFor='username'
+                    className='text-right'
+                  >
+                    Idea
+                  </Label>
+                  <Textarea className='text-black px-40' />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button
+                  className='border rounded'
+                  type='submit'
+                >
+                  Save
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </div>
